a136418f06fe6d1c1baa4cc1c3dabd7bacb8945f
lakshaygarg128 <lakshaygarg128@gmail.com>
2021-05-16 22:21:37 +0530

    splash screen

--- app/src/main/java/com/example/newsapp/Splashscreen.kt
+++ app/src/main/java/com/example/newsapp/Splashscreen.kt
@@ -1,11 +1,20 @@
 package com.example.newsapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.os.HandlerCompat.postDelayed
+import java.util.logging.Handler
 
 class Splashscreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splashscreen)
+
+        android.os.Handler().postDelayed({
+            val intent = Intent(this,CategoryList1::class.java)
+            startActivity(intent)
+        },3000)
+
     }
 }
