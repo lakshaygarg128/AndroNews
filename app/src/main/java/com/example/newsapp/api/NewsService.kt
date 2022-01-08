cb1ec7ab35f80de0e0806033594e3b627995cb76
lakshaygarg128 <lakshaygarg128@gmail.com>
2022-01-08 15:50:50 +0530

    Retrofit Added

--- app/src/main/java/com/example/newsapp/api/NewsService.kt
+++ app/src/main/java/com/example/newsapp/api/NewsService.kt
@@ -2,10 +2,14 @@ package com.example.newsapp.api
 
 import com.example.newsapp.models.NewsList
 import retrofit2.Call
+import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface NewsService {
-    @GET("/category")
-    suspend fun getNews(@Query("category") category: String): Call<NewsList>
+    @GET("/v2/top-headlines")
+    suspend fun getNews(@Query("country") country : String="in",
+                        @Query("category") category:String,
+                        @Query("apiKey") API_KEY : String="fc1ecaee0c064fea84c3b4f452debe24"
+    ): Response<NewsList>
 }
--- app/src/main/java/com/example/newsapp/api/retrofithelper.kt
+++ app/src/main/java/com/example/newsapp/api/retrofithelper.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object retrofithelper {
-    private const val base_url = "https://saurav.tech/NewsAPI/top-headlines"
+    private const val base_url = "https://newsapi.org"
 
 
        val instance =  Retrofit.Builder().baseUrl(base_url).addConverterFactory(GsonConverterFactory.create()).build().create(NewsService::class.java)
